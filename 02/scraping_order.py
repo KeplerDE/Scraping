3315e350c284f31c6496573e9c44598eb6b5c130
Dennny <keplerde@gmail.com>
2022-12-06 12:00:01 +0100

    Two

--- 02/scraping_order.py
+++ 02/scraping_order.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json  # импортируем чтобы сохранить данные в этот формат
+import csv
 
 from unicodedata import category
 
@@ -70,4 +71,73 @@ for category_name, category_href in all_categories.items():  # выбираем 
                       encoding="utf-8") as file:  # сохраним нашу страницу под именем категорий
                 file.write(src)
 
+            with open(f"data/{count}_{category_name}.html", encoding="utf-8") as file:
+                src = file.read()      # переделали метод на чтение
+
+            soup = BeautifulSoup(src, "lxml")
+
+            # собираем заголовки таблицы
+            table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+            # print(table_head)   # образуется обычный спсиок и мы можем обращаться по индексам
+            product = table_head[0].text
+            calories = table_head[1].text
+            proteins = table_head[2].text
+            fats = table_head[3].text
+            carbohydrates = table_head[4].text
+            # print(carbohydrates)
+
+            with open(f"data/{count}_{category_name}.csv", "w", encoding="utf-8") as file:  # открываем для записи
+                writer = csv.writer(file)           # файл будет записан с расширением csv, создали наш writer и передаем наш файл
+                writer.writerow(               # передали 5 аргументов за раз
+                    (
+                        product,
+                        calories,
+                        proteins,
+                        fats,
+                        carbohydrates
+                    )
+                )
+
+            # собираем данные продуктов
+            products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+            # print(products_data)
+
+            product_info = []
+            for item in products_data:
+                product_tds = item.find_all("td")
+
+                title = product_tds[0].find("a").text
+                calories = product_tds[1].text
+                proteins = product_tds[2].text
+                fats = product_tds[3].text
+                carbohydrates = product_tds[4].text
+                # print(proteins)
+                product_info.append(
+                    {
+                        "Title": title,
+                        "Calories": calories,
+                        "Proteins": proteins,
+                        "Fats": fats,
+                        "Carbohydrates": carbohydrates
+                    }
+                )
+            # вверху мы собрали данные, а теперь будем их записывать
+                with open(f"data/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(
+                        (
+                            title,
+                            calories,
+                            proteins,
+                            fats,
+                            carbohydrates
+                        )
+                    )
+            with open(f"data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
+                json.dump(product_info, file, indent=4, ensure_ascii=False)
             count += 1
+
+            print(f"# Итерация {count}. {category_name} записан...")
+
+
+
